613a4a651de986d11c7edfa5def157d0b5e00d33
Omry Hay <omry@env0.com>
2021-03-13 21:53:49 +0200

    adding K8S

---
+++ aws-blueprints/app-api-k8s/main.tf
@@ -0,0 +1,15 @@
+
+resource "null_resource" "app-api-k8s" {
+}
+
+variable "stage" {
+  type = string
+}
+
+output "app-api-k8s" {
+  value = "app-api-k8s"
+}
+
+output "stage" {
+  value = "stage"
+}
